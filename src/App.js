8dae3c51fd3e0c7c2ab9bec53bed2051813aaf61
Vikram Singh <mritxperts@gmail.com>
2021-10-18 17:20:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcom to itxperts</p>
+        <p>Welcom to itxperts <br>Vikram</br></p>
       </header>
     </div>
   );
